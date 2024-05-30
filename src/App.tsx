cfd6758766e99a2b0e5bc2dad10481d154c0ba5c
scripto1 <heeyongi@gmail.com>
2024-05-31 01:25:39 +0900

    feat: App수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 import PayrollDetails from "./components/PayrollDetails";
-import CorrectionRequest from "./components/CorrectionRequest";
-import RequestList from "./components/RequestList";
-import RequestManagement from "./components/RequestManagement";
+
 // import Login from "./components/Login";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
@@ -31,47 +23,10 @@ const App: React.FC = () => {
   }
 
   return (
-    <Router>
+    <>
       <GlobalStyles />
-      <Routes>
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route
-          path="/"
-          element={
-            <Navigate to={user ? "/payroll-details" : "/login"} replace />
-          }
-        />
-        <Route
-          path="/payroll-details"
-          element={
-            user ? (
-              <PayrollDetails
-                employee={sampleEmployee}
-                payroll={samplePayroll}
-              />
-            ) : (
-              <Navigate to="/login" replace />
-            )
-          }
-        />
-        <Route
-          path="/correction-request"
-          element={
-            user ? <CorrectionRequest /> : <Navigate to="/login" replace />
-          }
-        />
-        <Route
-          path="/request-list"
-          element={user ? <RequestList /> : <Navigate to="/login" replace />}
-        />
-        <Route
-          path="/request-management"
-          element={
-            user ? <RequestManagement /> : <Navigate to="/login" replace />
-          }
-        />
-      </Routes>
-    </Router>
+      <PayrollDetails employee={sampleEmployee} payroll={samplePayroll} />
+    </>
   );
 };
 
