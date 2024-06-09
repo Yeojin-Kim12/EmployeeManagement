4f8b948f025d1f718887bc899f8c98cc5aaa3c4d
SangHwa <kimsh9026@gmail.com>
2024-06-09 11:11:32 +0900

    temp: before pull

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  console.log("Private");
   const user = useSelector((state: RootState) => state.auth.user);
   return user ? children : <Navigate to="/login" />;
 };
--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 
 const PublicRoute = ({ children }: { children: JSX.Element }) => {
+  console.log('public');
   const user = useSelector((state: RootState) => state.auth.user);
   return user ? <Navigate to="/profile" /> : children;
 };
